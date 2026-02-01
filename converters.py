57f18a670174f8b291b4dbb5d9c00dc7305243a0
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2026-02-01 11:12:51 +0330

    Made morse_validator more strict && added more dt annotations

--- converters.py
+++ converters.py
@@ -12,19 +12,25 @@ class Converters:
         self.ascii_to_morse_dict: dict[str: str] = MorseCodeDataset.morse_dict
         self.morse_to_ascii_dict: dict[str: str] = {v: k for k, v in MorseCodeDataset.morse_dict.items()}
 
-    def is_morse_valid(self, morse_code) -> bool:
+    def is_morse_valid(self, morse_code: str) -> bool:
+        if len(morse_code) == 0: return False
         for char in morse_code:
             if char not in ['.', '-', ' ', '/']:
                 return False
+        for token in morse_code.split(' '):
+            if len(token) > 7:
+                return False
+            if token.startswith('/') and len(token) > 1:
+                return False
         return True
 
-    def string_tokenizer(self, string, delimiter) -> list[str]:
+    def string_tokenizer(self, string: str, delimiter: str) -> list[str]:
         tokens: list[str] = []
         for token in string.split(delimiter):
             tokens.append(token)
         return tokens
 
-    def text_to_morse(self, text) -> str:
+    def text_to_morse(self, text: str) -> str:
         tokens: list[str] = self.string_tokenizer(text.upper(), ' ')
         translated_tokens: list[str] = []
         for token in tokens:
@@ -38,7 +44,7 @@ class Converters:
             translated_tokens.append(" ".join(translated_chars))
         return " / ".join(translated_tokens)
     
-    def morse_to_text(self, morse_code) -> str:
+    def morse_to_text(self, morse_code: str) -> str:
         tokens: list[str] = self.string_tokenizer(morse_code, ' / ')
         translated_tokens: list[str] = []
         for token in tokens:
