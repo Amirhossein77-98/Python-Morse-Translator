f26ef6d9366f1872999349e7e757efb38a32d097
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2026-02-01 12:33:47 +0330

    Added cl args for inline scripting && added morse_validator option && separated message strings in another dataclass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from converters import Converters
+import argparse
+from messages import ErrorMessages, MenuMessages, OutputMessages
 
 def main():
     """
@@ -13,46 +15,70 @@ def main():
     Raises:
         ValueError: Handled internally for invalid numeric input during menu selection.
     """
-    converter = Converters()
-    print(f"\n{'*'*5} Morse Code Translator {'*'*5} \n")
-    while True:
-        print("Please choose what you want to do (enter the option number only): ")
-        print("1. Text to Morse Code")
-        print("2. Morse Code to Text")
-        print("3. Quit")
-        
-        user_choice: int
-        while True:
-            try:
-                user_choice = int(input("> Your choice: "))
-                if user_choice in [1, 2, 3]:
-                    break
-                else:
-                    print("Invalid choice. Please enter 1, 2 or 3.")
-            except ValueError:
-                print("Invalid input. Please enter a number (1, 2 or 3).")
-                continue
-        
-        if user_choice == 3:
-            print("\n##### Exiting the program. Goodbye!\n")
-            break
 
+    converter = Converters()
+    parser = argparse.ArgumentParser(description="An argument parser to use the code as cli script.")
+    parser.add_argument('-m', '--morse', help="To enter morse directly and translate.")
+    parser.add_argument('-t', '--text', help="To enter text directly and translate.")
+    parser.add_argument('-vm', '--validate_morse', help="To check if a morse code is valid.")
+    args = parser.parse_args()
+    
+    print(f"\n{'*'*5} Morse Code Translator {'*'*5} \n")
+    if args.validate_morse:
+        if args.morse or args.text:
+            print(ErrorMessages.invalid_input_get_cli_help)
+        else:
+            print(OutputMessages.valid_morse if converter.is_morse_valid(args.validate_morse) else OutputMessages.invalid_morse)
+    if args.morse and args.text:
+        print(ErrorMessages.invalid_input_get_cli_help)
+    if args.morse or args.text:
+        if args.morse and converter.is_morse_valid(args.morse):
+            print(OutputMessages.original_morse + args.morse)
+            print(OutputMessages.translated_text + converter.morse_to_text(args.morse).title() + "\n")
+        elif args.text:
+            print(OutputMessages.original_text + args.text)
+            print(OutputMessages.translated_morse + converter.text_to_morse(args.text) + "\n")
+        else:
+            print(ErrorMessages.invalid_input)
+    else:
         while True:
-            print("\n!!!CAUTION: Please use ' / ' as 'space' or seperator between words and simple space (' ') to separate characters.") if user_choice == 2 else ""
-            user_input: str = input(f"\n> Enter your {"Text" if user_choice == 1 else "Morse Code"}: ").strip()
-            if user_choice == 1:
-                print(f"\n>>> Original Text: {user_input}")
-                print(f">>> Translated Morse: {converter.text_to_morse(text=user_input)}")
-                print(f"\n{'='*35}\n")
+            print(MenuMessages.menu_prompt)
+            print(MenuMessages.menu_options)
+            
+            user_choice: int
+            while True:
+                try:
+                    user_choice = int(input(MenuMessages.user_choice_input_msg))
+                    if user_choice in [1, 2, 3, 4]:
+                        break
+                    else:
+                        print(ErrorMessages.invalid_menu_option_choice)
+                except ValueError:
+                    print(ErrorMessages.invalid_menu_option_choice)
+                    continue
+            
+            if user_choice == 4:
+                print(MenuMessages.exiting_program_msg)
                 break
-            elif user_choice == 2 and converter.is_morse_valid(user_input):
-                print(f"\n>>> Original Morse Code: {user_input}")
-                print(f">>> Translated Text: {converter.morse_to_text(morse_code=user_input).title()}")
-                print(f"\n{'='*35}\n")
-                break
-            else:
-                print("Invalid Morse Code input. Please try again.")
-                continue
+
+            while True:
+                print(MenuMessages.morse_caution) if user_choice == 2 else ""
+                user_input: str = input(f"\n> Enter your {"Text" if user_choice == 1 else "Morse Code"}: ").strip()
+                if user_choice == 1:
+                    print("\n" + OutputMessages.original_text + user_input)
+                    print(OutputMessages.translated_morse + converter.text_to_morse(text=user_input))
+                    print(f"\n{'='*35}\n")
+                    break
+                elif user_choice == 2 and converter.is_morse_valid(user_input):
+                    print("\n" + OutputMessages.original_morse + user_input)
+                    print(OutputMessages.translated_text + converter.morse_to_text(morse_code=user_input).title())
+                    print(f"\n{'='*35}\n")
+                    break
+                elif user_choice == 3:
+                    print(OutputMessages.valid_morse if converter.is_morse_valid(user_input) else OutputMessages.invalid_morse)
+                else:
+                    print(ErrorMessages.invalid_morse_input)
+                    continue
 
 if __name__ == "__main__":
     main()
---
+++ messages.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from textwrap import dedent
+
+@dataclass
+class ErrorMessages:
+    invalid_input_get_cli_help: str = "Invalid Input. Please use '--help' to find cli args."
+    invalid_input: str = "Invalid Input."
+    invalid_menu_option_choice: str = "Invalid choice/input. Please enter 1, 2, 3 or 4."
+    invalid_morse_input: str = "Invalid Morse Code input. Please try again."
+
+@dataclass
+class MenuMessages:
+    menu_prompt: str = "Please choose what you want to do (enter the option number only): "
+    menu_options: str = dedent("""
+    1. Text to Morse Code
+    2. Morse Code to Text
+    3. Validate Morse Code
+    4. Quit
+    """)
+    user_choice_input_msg: str = "> Your choice: "
+    exiting_program_msg: str = "\n##### Exiting the program. Goodbye!\n"
+    morse_caution = "\n!!!CAUTION: Please use ' / ' as 'space' or seperator between words and simple space (' ') to separate characters."
+
+@dataclass
+class OutputMessages:
+    original_text: str = ">>> Original Text: "
+    original_morse: str = ">>> Original Morse Code: "
+    translated_text: str = ">>> Translated Text: "
+    translated_morse: str = ">>> Translated Morse Code: "
+    valid_morse: str = ">>> It's Valid Morse."
+    invalid_morse: str = ">>> It's NOT a Valid Morse."
