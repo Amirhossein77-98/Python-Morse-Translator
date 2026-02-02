c4d6aa35cbde3173519680d6c4bc00de0ae7a209
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2026-02-02 21:53:57 +0330

    Added POST functions to api for multi-word support

--- api/app.py
+++ api/app.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI, HTTPException, status
 from core.converters import Converters
 from views.messages import AppName, APIMessages, ErrorMessages
 from utilities.utils import HelperFunctions
+from api.schemas import TextRequest, MorseRequest
 
 app = FastAPI()
 converter: Converters = Converters()
 
+# GET Routes
 @app.get('/')
 def root():
     return {"message": AppName.app_name}
@@ -37,6 +39,35 @@ def text_to_morse(text: str):
 @app.get('/validate-morse/{morse}')
 def validate_morse(morse: str):
     morse = morse.strip()
+    return {
+        APIMessages.api_original_morse_key: morse,
+        APIMessages.api_morse_validation_check_key: HelperFunctions.is_morse_valid(morse)
+    }
+
+# POST Routs
+@app.post('/morse-to-text')
+def morse_to_text_post(body: MorseRequest):
+    morse = body.morse.strip()
+    if HelperFunctions.is_morse_valid(morse):
+        return {
+            APIMessages.api_original_morse_key: morse,
+            APIMessages.api_translated_morse_key: converter.morse_to_text(morse)
+            }
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=ErrorMessages.invalid_morse_input)
+
+@app.post('/text-to-morse')
+def text_to_morse_post(body: TextRequest):
+    text = body.text.strip()
+    if len(text) != 0:
+        return {
+            APIMessages.api_original_text_key: text,
+            APIMessages.api_translated_text_key: converter.text_to_morse(text)
+        }
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=ErrorMessages.invalid_input)
+
+@app.get('/validate-morse')
+def validate_morse_post(body: MorseRequest):
+    morse = body.morse.strip()
     return {
         APIMessages.api_original_morse_key: morse,
         APIMessages.api_morse_validation_check_key: HelperFunctions.is_morse_valid(morse)
