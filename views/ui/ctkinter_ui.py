f1b551f0f82e1b0369fecdd5795c9bc025222d4c
Amirhossein Gholizadeh <Amirhosseingholizadeh1998@gmail.com>
2026-02-04 17:55:24 +0330

    GUI Dataclass name change

--- views/ui/ctkinter_ui.py
+++ views/ui/ctkinter_ui.py
@@ -6,7 +6,7 @@ from views.messages import ErrorMessages, APIMessages
 from dataclasses import dataclass
 
 @dataclass
-class ThemeConstants:
+class GUIConstants:
     app_font_family: str = "Arial"
     title_font: tuple = (app_font_family, 60, "bold")
     switch_font: tuple = (app_font_family, 35)
@@ -47,13 +47,13 @@ class App(ctk.CTk):
         
         self.converter = Converters()
         
-        ctk.set_appearance_mode(ThemeConstants.theme_values[0])
-        ctk.set_default_color_theme(ThemeConstants.default_color_theme)
+        ctk.set_appearance_mode(GUIConstants.theme_values[0])
+        ctk.set_default_color_theme(GUIConstants.default_color_theme)
         
-        self.geometry(ThemeConstants.window_default_size)
+        self.geometry(GUIConstants.window_default_size)
         self.minsize(900, 1000)
         
-        self.title(ThemeConstants.app_title)
+        self.title(GUIConstants.app_title)
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=0)
@@ -65,7 +65,7 @@ class App(ctk.CTk):
         self.grid_rowconfigure(4, weight=1)
         self.grid_rowconfigure(5, weight=1)
 
-        self.title_label = ctk.CTkLabel(self, text=AppName.app_name, font=ThemeConstants.title_font)
+        self.title_label = ctk.CTkLabel(self, text=AppName.app_name, font=GUIConstants.title_font)
         self.title_label.grid(row=0, column=1, pady=20, sticky="nsew")
 
         self.center_frame = ctk.CTkFrame(self, fg_color="transparent")
@@ -74,32 +74,32 @@ class App(ctk.CTk):
         self.center_frame.grid_columnconfigure(1, weight=0)
         self.center_frame.grid_columnconfigure(2, weight=0)
 
-        self.left_label = ctk.CTkLabel(self.center_frame, text="Text to Morse", font=ThemeConstants.switch_font)
+        self.left_label = ctk.CTkLabel(self.center_frame, text="Text to Morse", font=GUIConstants.switch_font)
         self.left_label.grid(row=0, column=0, padx=(0, 8), sticky="e")
 
         self.operation_type_switch_var = ctk.StringVar(value="text-to-morse")
         self.operation_type_switch = ctk.CTkSwitch(self.center_frame,
-                                              offvalue=ThemeConstants.switch_keys["ttm"],
-                                              onvalue=ThemeConstants.switch_keys["mtt"],
+                                              offvalue=GUIConstants.switch_keys["ttm"],
+                                              onvalue=GUIConstants.switch_keys["mtt"],
                                               variable=self.operation_type_switch_var,
                                               switch_width=100,
                                               switch_height=50,
                                               text="",
-                                              fg_color=ThemeConstants.translation_direction_switch_theme_color["off"],
-                                              progress_color=ThemeConstants.translation_direction_switch_theme_color["on"],
-                                              button_color=ThemeConstants.translation_direction_switch_theme_color["btn"],
-                                              button_hover_color=ThemeConstants.translation_direction_switch_theme_color["btnh"])
+                                              fg_color=GUIConstants.translation_direction_switch_theme_color["off"],
+                                              progress_color=GUIConstants.translation_direction_switch_theme_color["on"],
+                                              button_color=GUIConstants.translation_direction_switch_theme_color["btn"],
+                                              button_hover_color=GUIConstants.translation_direction_switch_theme_color["btnh"])
         self.operation_type_switch.grid(row=0, column=1, padx=8)
 
-        self.right_label = ctk.CTkLabel(self.center_frame, text="Morse to Text", font=ThemeConstants.switch_font)
+        self.right_label = ctk.CTkLabel(self.center_frame, text="Morse to Text", font=GUIConstants.switch_font)
         self.right_label.grid(row=0, column=2, padx=(8, 0), sticky="w")
 
         self.input_field = ctk.CTkTextbox(self,
         width=800,
         height=200,
-        font=ThemeConstants.io_font,
+        font=GUIConstants.io_font,
         corner_radius=30,
-        border_color=ThemeConstants.io_border_color,
+        border_color=GUIConstants.io_border_color,
         border_width=1)
         self.input_field.insert("1.0", f"Enter your text here...{chr(0x200B)}")
         self.input_field.configure(text_color=("gray", "gray"))
@@ -110,9 +110,9 @@ class App(ctk.CTk):
         self.output_field = ctk.CTkTextbox(self,
         width=800,
         height=200,
-        font=ThemeConstants.io_font,
+        font=GUIConstants.io_font,
         corner_radius=30,
-        border_color=ThemeConstants.io_border_color,
+        border_color=GUIConstants.io_border_color,
         border_width=1)
         self.output_field.grid(row=3, column=0, columnspan=3, pady=(0, 20), padx=20, sticky="n")
         self.output_field.insert("1.0", "Translation")
@@ -123,7 +123,7 @@ class App(ctk.CTk):
         text="Translate",
         width=200,
         height=90,
-        font=ThemeConstants.other_font,
+        font=GUIConstants.other_font,
         corner_radius=50,
         command=self.transate_button)
         self.translate_button.grid(row=4, column=1, sticky="n")
@@ -136,19 +136,19 @@ class App(ctk.CTk):
         self.bottom_frame.grid_columnconfigure(1, weight=1)
         self.bottom_frame.grid_columnconfigure(2, weight=1)
         
-        self.mode_selector_label = ctk.CTkLabel(self.bottom_frame, text="Mode:", font=ThemeConstants.other_font)
+        self.mode_selector_label = ctk.CTkLabel(self.bottom_frame, text="Mode:", font=GUIConstants.other_font)
         self.mode_selector_label.grid(row=0, column=0, padx=(20, 0), sticky="sw")
 
-        self.translation_mode_var = ctk.StringVar(value=ThemeConstants.translation_mode_values[0])
+        self.translation_mode_var = ctk.StringVar(value=GUIConstants.translation_mode_values[0])
         self.mode_selection_menu = ctk.CTkOptionMenu(self.bottom_frame,
-                                                      values=ThemeConstants.translation_mode_values,
+                                                      values=GUIConstants.translation_mode_values,
                                                       variable=self.translation_mode_var,
                                                       width=200,
                                                       height=50,
-                                                      font=ThemeConstants.other_font,
-                                                      dropdown_font=ThemeConstants.other_font,
-                                                      fg_color=ThemeConstants.option_menu_theme_colors["dark"]["fg"],
-                                                      button_color=ThemeConstants.option_menu_theme_colors["dark"]["btn"],
+                                                      font=GUIConstants.other_font,
+                                                      dropdown_font=GUIConstants.other_font,
+                                                      fg_color=GUIConstants.option_menu_theme_colors["dark"]["fg"],
+                                                      button_color=GUIConstants.option_menu_theme_colors["dark"]["btn"],
                                                       dynamic_resizing=False,
                                                       command=self.change_translation_mode)
         self.mode_selection_menu.grid(row=1, column=0, padx=(20, 0), sticky="sw")
@@ -156,36 +156,36 @@ class App(ctk.CTk):
         self.api_switcher_var = ctk.StringVar(value="off")
         self.api_switch_label = ctk.CTkLabel(self.bottom_frame,
                                              text=f"API State: {self.api_switcher_var.get().title()}",
-                                             font=ThemeConstants.other_font)
+                                             font=GUIConstants.other_font)
         self.api_switch_label.grid(row=0, column=1)
 
         self.api_switch = ctk.CTkSwitch(self.bottom_frame,
-                                              offvalue=ThemeConstants.api_states[1],
-                                              onvalue=ThemeConstants.api_states[0],
+                                              offvalue=GUIConstants.api_states[1],
+                                              onvalue=GUIConstants.api_states[0],
                                               variable=self.api_switcher_var,
                                               switch_width=70,
                                               switch_height=40,
                                               text="",
                                               command=self.change_api_state,
-                                              fg_color=ThemeConstants.api_state_switch_theme_color["off"],
-                                              progress_color=ThemeConstants.api_state_switch_theme_color["on"],
-                                              button_color=ThemeConstants.api_state_switch_theme_color["btn"],
-                                              button_hover_color=ThemeConstants.api_state_switch_theme_color["btnh"])
+                                              fg_color=GUIConstants.api_state_switch_theme_color["off"],
+                                              progress_color=GUIConstants.api_state_switch_theme_color["on"],
+                                              button_color=GUIConstants.api_state_switch_theme_color["btn"],
+                                              button_hover_color=GUIConstants.api_state_switch_theme_color["btnh"])
         self.api_switch.grid(row=1, column=1)
 
-        self.theme_selector_label = ctk.CTkLabel(self.bottom_frame, text=":Theme", font=ThemeConstants.other_font)
+        self.theme_selector_label = ctk.CTkLabel(self.bottom_frame, text=":Theme", font=GUIConstants.other_font)
         self.theme_selector_label.grid(row=0, column=2, padx=(0, 20), sticky="se")
 
         self.theme_var = ctk.StringVar(value="System")
         self.theme_selection_menu = ctk.CTkOptionMenu(self.bottom_frame,
-                                                      values=ThemeConstants.theme_values,
+                                                      values=GUIConstants.theme_values,
                                                       variable=self.theme_var,
                                                       width=200,
                                                       height=50,
-                                                      font=ThemeConstants.other_font,
-                                                      dropdown_font=ThemeConstants.other_font,
-                                                      fg_color=ThemeConstants.option_menu_theme_colors["dark"]["fg"],
-                                                      button_color=ThemeConstants.option_menu_theme_colors["dark"]["btn"],
+                                                      font=GUIConstants.other_font,
+                                                      dropdown_font=GUIConstants.other_font,
+                                                      fg_color=GUIConstants.option_menu_theme_colors["dark"]["fg"],
+                                                      button_color=GUIConstants.option_menu_theme_colors["dark"]["btn"],
                                                       dynamic_resizing=False,
                                                       command=self.change_theme)
         self.theme_selection_menu.grid(row=1,column=2, padx=(0, 20), sticky="se")
@@ -195,7 +195,7 @@ class App(ctk.CTk):
         input_text = self.input_field.get("1.0", "end").strip()
         if not input_text or input_text == f"Enter your text here...{chr(0x200B)}":
             result = ""
-        elif self.translation_mode_var.get() == ThemeConstants.translation_mode_values[0]:
+        elif self.translation_mode_var.get() == GUIConstants.translation_mode_values[0]:
             result = self.offline_translator(input_text)
         else:
             result = self.online_translator(input_text)
@@ -208,7 +208,7 @@ class App(ctk.CTk):
         self.output_field.configure(state="disabled")
 
     def offline_translator(self, input_text: str) -> str:
-        if self.operation_type_switch_var.get() == ThemeConstants.switch_keys["ttm"]:
+        if self.operation_type_switch_var.get() == GUIConstants.switch_keys["ttm"]:
             return self.converter.text_to_morse(input_text)
         else:
             if HelperFunctions.is_morse_valid(input_text):
@@ -220,8 +220,8 @@ class App(ctk.CTk):
         try:
             import requests
             base_url: str = "http://127.0.0.1:8000/v1/"
-            if self.operation_type_switch_var.get() == ThemeConstants.switch_keys["ttm"]:
-                url_param: str = ThemeConstants.switch_keys["ttm"]
+            if self.operation_type_switch_var.get() == GUIConstants.switch_keys["ttm"]:
+                url_param: str = GUIConstants.switch_keys["ttm"]
                 result = requests.post(
                     url=base_url+url_param,
                     json={"text": input_text}
@@ -233,7 +233,7 @@ class App(ctk.CTk):
                     return result_json["detail"]
             else:
                 if HelperFunctions.is_morse_valid(input_text):
-                    url_param: str = ThemeConstants.switch_keys["mtt"]
+                    url_param: str = GUIConstants.switch_keys["mtt"]
                     result = requests.post(
                         url=base_url+url_param,
                         json={"morse": input_text}
@@ -250,26 +250,26 @@ class App(ctk.CTk):
 
     def change_theme(self, choice):
         ctk.set_appearance_mode(choice)
-        if str(ctk.get_appearance_mode()).lower() == ThemeConstants.theme_values[2].lower():
+        if str(ctk.get_appearance_mode()).lower() == GUIConstants.theme_values[2].lower():
             self.theme_selection_menu.configure(
-                                                fg_color=ThemeConstants.option_menu_theme_colors["light"]["fg"],
-                                                button_color=ThemeConstants.option_menu_theme_colors["light"]["btn"],
+                                                fg_color=GUIConstants.option_menu_theme_colors["light"]["fg"],
+                                                button_color=GUIConstants.option_menu_theme_colors["light"]["btn"],
                                                 text_color="black",
                                                 )
             self.mode_selection_menu.configure(
-                                                fg_color=ThemeConstants.option_menu_theme_colors["light"]["fg"],
-                                                button_color=ThemeConstants.option_menu_theme_colors["light"]["btn"],
+                                                fg_color=GUIConstants.option_menu_theme_colors["light"]["fg"],
+                                                button_color=GUIConstants.option_menu_theme_colors["light"]["btn"],
                                                 text_color="black",
                                                 )
         else:
             self.theme_selection_menu.configure(
-                                                fg_color=ThemeConstants.option_menu_theme_colors["dark"]["fg"],
-                                                button_color=ThemeConstants.option_menu_theme_colors["dark"]["btn"],
+                                                fg_color=GUIConstants.option_menu_theme_colors["dark"]["fg"],
+                                                button_color=GUIConstants.option_menu_theme_colors["dark"]["btn"],
                                                 text_color="white",
                                                 )
             self.mode_selection_menu.configure(
-                                                fg_color=ThemeConstants.option_menu_theme_colors["dark"]["fg"],
-                                                button_color=ThemeConstants.option_menu_theme_colors["dark"]["btn"],
+                                                fg_color=GUIConstants.option_menu_theme_colors["dark"]["fg"],
+                                                button_color=GUIConstants.option_menu_theme_colors["dark"]["btn"],
                                                 text_color="white",
                                                 )
     def on_textbox_entry(self, event):
@@ -288,7 +288,7 @@ class App(ctk.CTk):
 
     def change_api_state(self):
         import threading
-        if self.api_switcher_var.get() == ThemeConstants.api_states[0]:
+        if self.api_switcher_var.get() == GUIConstants.api_states[0]:
             threading.Thread(target=self.start_api, daemon=True).start()
         else:
             self.stop_api()
@@ -308,7 +308,7 @@ class App(ctk.CTk):
             self.api_switch_label.configure(text=f"API State: {self.api_switcher_var.get().title()}")
         except Exception as e:
             print("Failed to start API:", e)
-            self.api_switcher_var = ThemeConstants.api_states[1]
+            self.api_switcher_var = GUIConstants.api_states[1]
             self.api_switch.deselect()
 
     def api_stopper(self):
